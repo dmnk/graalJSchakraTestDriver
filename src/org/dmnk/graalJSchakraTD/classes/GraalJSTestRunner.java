234fb097a7c27791c218f358ec4dee32dcdaf165
dmnk <d0m1n1k@gmx.net>
2016-07-28 14:07:00 +0200

    utilize the new config provider and cleaner flow / reorganization

--- src/org/dmnk/graalJSchakraTD/classes/GraalJSTestRunner.java
+++ src/org/dmnk/graalJSchakraTD/classes/GraalJSTestRunner.java
@@ -4,16 +4,18 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.Map.Entry;
 
-import org.dmnk.graalJSchakraTD.classes.Configuration.ExecutableMode;
+import org.dmnk.graalJSchakraTD.classes.Configuration.ListMode;
 import org.dmnk.graalJSchakraTD.classes.configProvider.CLIConfigProvider;
+import org.dmnk.graalJSchakraTD.classes.configProvider.DefaultConfigProvider;
 import org.dmnk.graalJSchakraTD.classes.export.HTMLResultExporter;
 import org.dmnk.graalJSchakraTD.classes.export.TextResultExporter;
 import org.dmnk.graalJSchakraTD.exceptions.GraalJSTestException;
+import org.dmnk.graalJSchakraTD.interfaces.ConfigurationProviderInterface;
 import org.dmnk.graalJSchakraTD.interfaces.ExecutedTest;
 import org.dmnk.graalJSchakraTD.interfaces.FailedTest;
+import org.dmnk.graalJSchakraTD.interfaces.ListFetcher;
 import org.dmnk.graalJSchakraTD.interfaces.PassedTest;
 import org.dmnk.graalJSchakraTD.interfaces.ResultExporter;
 import org.dmnk.graalJSchakraTD.interfaces.TestInitiator;
@@ -26,34 +28,20 @@ public class GraalJSTestRunner {
 	private List<ResultExporter> resExp;
 	private List<TestGroup> tests;
 	private List<TestExecutedGroup> executedTests;
+	private Map<String, HashMap<String, Integer>> crashlist;
+	private Map<String, Integer> folderList;
 	private TestInitiator testInit;
 	private TestFetcher tf;
+	private ListFetcher lf;
+	private Configuration conf;
 
 	public static void main(String[] args) {
 		
-		Configuration conf = new CLIConfigProvider(args).getConfig();
-		
-		conf.setGraalJSexec(Configuration.ExecutableMode.DIRECT, "../../GraalVM-0.10/bin/js");
-		conf.setChakraTestsPath("../../GraalVM-0.10/chakraTests/test/");
-		conf.addExport("html", "./data/htmlResult.html");
-		conf.addExport("csv", "./data/FailPass.csv");
-		
 		GraalJSTestRunner ctr = new GraalJSTestRunner();
+		
 		try {
-			if(conf.checkExport("html")) {
-				ResultExporter hre = new HTMLResultExporter(conf.getExport("html"));
-				ctr.addResultExporter(hre);
-			}
-			if (conf.checkExport("csv")) {
-				ResultExporter tre = new TextResultExporter(conf.getExport("csv"));
-//				ctr.addResultExporter(tre);
-			}
-			TestInitiator ti = new GraalJSTestInitiator(conf.getGraalJSexec());
-			TestFetcher tf = new GraalJSTestFetcher(conf.getChakraTestsPath());
-						
-			ctr.setTestInitiator(ti);
-			ctr.setTestFetcher(tf);
-			ctr.run(args);
+			ctr.setup(args);
+			ctr.run();
 		} catch (GraalJSTestException e) {
 			System.err.println(e.getMessage());
 			System.exit(-1);
@@ -64,6 +52,32 @@ public class GraalJSTestRunner {
 	public GraalJSTestRunner() {
 		resExp = new LinkedList<ResultExporter>();
 		executedTests = new LinkedList<TestExecutedGroup>();
+	}
+	
+	private void setupConfig(String [] args) {
+		conf = new Configuration(); //place for default config, CLIprovider should append/replace settings
+		ConfigurationProviderInterface defConf, cliConf;
+		defConf = new DefaultConfigProvider();
+		defConf.workOn(conf);
+		try {
+			conf = defConf.getConfig();
+		} catch (Exception e) {
+			//e.printStackTrace(); //def conf can't thow an exception
+		}
+		
+		cliConf = new CLIConfigProvider(args);
+		cliConf.workOn(conf);
+		
+		try {
+			conf =cliConf.getConfig();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+				
+		if(!conf.readyToExec()) {
+			System.exit(0);
+		}
 	}
 	
 	private void addResultExporter(ResultExporter re) {
@@ -77,53 +91,83 @@ public class GraalJSTestRunner {
 	private void setTestFetcher(TestFetcher tf) {
 		this.tf = tf;
 	}
+	
+	private void setListFetcher(ListFetcher lf) {
+		this.lf = lf;
+	}
+	
+	private void setup(String[] args) throws GraalJSTestException {
+		setupConfig(args);
 
-	private void run(String[] args) {
-		//check preconditions (propably in the constructors of TestFetcher, TestRunner, ...)
-
+		TestInitiator ti = new GraalJSTestInitiator(conf.getExec());
+		TestFetcher tf = new GraalJSTestFetcher(conf.getTestsPath());
+		ListFetcher lf = new GraalJSListFetcher(conf);
+					
+		setTestInitiator(ti);
+		setTestFetcher(tf);
+		setListFetcher(lf);
+		
+		fetchLists();
+		fetchTests();
+		
+		if(conf.checkExport("html")) {
+			ResultExporter hre = new HTMLResultExporter(conf.getExport("html"));
+			addResultExporter(hre);
+		}
+		if (conf.checkExport("csv")) {
+			ResultExporter tre = new TextResultExporter(conf.getExport("csv"));
+//			ctr.addResultExporter(tre);
+		}
+	}
+	
+	private void fetchTests() {
 		//get available tests from filesystem
 		tests = tf.fetch(); //FromDir(this.getChakraPath());
-		
+	}
+	
+	private void fetchLists() {
 		//process white/black/crashlists
-		Map<String, HashMap<String, Integer>> crashlist = new HashMap<String, HashMap<String, Integer>>();
-		HashMap<String, Integer> arrayexclusionlist = new HashMap<String, Integer>();
-		arrayexclusionlist.put("array_init.js", 1);
-		arrayexclusionlist.put("CopyOnAccessArray_cache_index_overflow.js", 1);
-		crashlist.put("Array", arrayexclusionlist);
+		crashlist = lf.fetchFileList(conf.getGrayList());
 		
-		Map<String, Integer> folderList = new HashMap<String, Integer>();
-		folderList.put("Array", 0);
-//		folderList.put("Basics", 0);
-//		folderList.put("Closures", 0);
-//		folderList.put("ControlFlow", 0);
-//		folderList.put("Date", 0);
+		Helper.debugOut(conf, 1, "TR", crashlist.size()+"");
+		for(Entry<String, HashMap<String, Integer>> entry : crashlist.entrySet()) {
+			Helper.debugOut(conf, 1, "TR-entry", entry.getKey());
+		}
 		
-		String mode = "WL"; //BL
-		
+		try {
+			if(conf.getListMode() == ListMode.WHITE) {
+				folderList = lf.fetchFolderList(conf.getWhiteList());
+			} else {
+				folderList = lf.fetchFolderList(conf.getBlackList());
+			}
+		} catch (Exception e) {
+			//...
+		}
+	}
+	
+
+	private void run() {
+		//check preconditions (propably in the constructors of TestFetcher, TestRunner, ...)
+
 		//execute the enabled tests
 		for(TestGroup tg : tests) {
 			if(tg == null) {
 				continue;
 				//actually, that shouldn't happen
 			}
-			switch (mode) {
-			case "WL":
-				if(!folderList.containsKey(tg.getGroupName())) {
-					continue;
-				}
-				break ;
-			case "BL":
-				if(folderList.containsKey(tg.getGroupName())) {
-					continue;
-				}
-				break;				
+			
+			//check if folder is black- / whitelisted
+			if(!groupActive(tg)) {
+				//back to loop
+				continue;
 			}
+			
 			TestExecutedGroup etg = new GraalJSTestExecutedGroup(tg.getGroupName());
 			executedTests.add(etg);
 			
 			for(Test t : tg.getTests()) {
-				
-				if(!arrayexclusionlist.containsKey(t.getTestName())) {
+				//check if file is graylisted
+				if(fileActive(tg, t)) {
 					ExecutedTest et = testInit.runTest(t);
 					etg.addTest(et);
 					
@@ -148,5 +192,53 @@ public class GraalJSTestRunner {
 		}
 	}
 	
+	/**
+	 * checks if the folder is ok (not in black/ in whitelist)
+	 * 
+	 * @param tg
+	 * @return
+	 */
+	private boolean groupActive(TestGroup tg) {
+		switch (conf.getListMode()) {
+		case WHITE:
+			if(folderList.containsKey(tg.getGroupName())) {
+				return true;
+			} else {
+				return false;
+			}
+//			break;
+		case BLACK:
+			if(folderList.containsKey(tg.getGroupName())) {
+				return false;
+			} else {
+				return true;
+			}
+		default: return false;
+		}
+	}
 	
+	/**
+	 * checks if the folder is ok (not in black/ in whitelist)
+	 * and if the test is ok (not graylisted)
+	 * @param tg
+	 * @param t
+	 * @return should the test be executed
+	 */
+	private boolean fileActive(TestGroup tg, Test t) {
+		if(!groupActive(tg)) {
+			return false;
+		}
+		if(crashlist.containsKey(tg.getGroupName())) {
+			HashMap<String, Integer> grayFolder = crashlist.get(tg.getGroupName());
+			if(grayFolder.containsKey(t.getTestName())) {
+				return false;
+			} else {
+				//other files in the folder are greylisted
+				return true;
+			}
+		} else {
+			return true;
+		}
+	}
+		
 }
