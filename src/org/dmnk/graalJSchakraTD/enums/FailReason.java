b786f452c8491d1b86713715a8023795d867da19
dmnk <d0m1n1k@gmx.net>
2016-08-20 10:37:25 +0200

    mini comments for the enums

--- src/org/dmnk/graalJSchakraTD/enums/FailReason.java
+++ src/org/dmnk/graalJSchakraTD/enums/FailReason.java
@@ -1,5 +1,10 @@
 package org.dmnk.graalJSchakraTD.enums;
 
+/**
+ * the possible failreasons, central place to define them globally
+ * @author Dominik
+ *
+ */
 public enum FailReason {
 	EXCEPTION, WARNING, ASSERTION, OUTPUT, CRASH;
 }
--- src/org/dmnk/graalJSchakraTD/enums/TestType.java
+++ src/org/dmnk/graalJSchakraTD/enums/TestType.java
@@ -1,5 +1,10 @@
 package org.dmnk.graalJSchakraTD.enums;
 
+/**
+ * the possible testtypes, central place to manage them globally
+ * @author Dominik
+ *
+ */
 public enum TestType {
 	BASELINE, PASSSTRING;
 }
