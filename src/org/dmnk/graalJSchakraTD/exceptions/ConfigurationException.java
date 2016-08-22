6d1e6c9008c84dc4fd48e1c6401fc2581a561ec1
dmnk <d0m1n1k@gmx.net>
2016-08-22 12:07:07 +0200

    class comments for the exceptions

--- src/org/dmnk/graalJSchakraTD/exceptions/ConfigurationException.java
+++ src/org/dmnk/graalJSchakraTD/exceptions/ConfigurationException.java
@@ -3,7 +3,9 @@ package org.dmnk.graalJSchakraTD.exceptions;
 public class ConfigurationException extends Exception {
 
 	/**
+	 * thrown when a problem either with, or caused by, the configuration occurs.
 	 * 
+	 * @author Dominik
 	 */
 	private static final long serialVersionUID = 1L;
 
--- src/org/dmnk/graalJSchakraTD/exceptions/TestException.java
+++ src/org/dmnk/graalJSchakraTD/exceptions/TestException.java
@@ -3,7 +3,9 @@ package org.dmnk.graalJSchakraTD.exceptions;
 public class TestException extends Exception {
 
 	/**
-	 * 
+	 * thrown when a problem with a test, or during testing occurs, thats not
+	 * related to the configuration
+	 * @author Dominik
 	 */
 	private static final long serialVersionUID = 1L;
 
